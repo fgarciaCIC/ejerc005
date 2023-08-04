06740c96209590763d9d4d9bf9d66c6f2b1ac808
fgarciaCIC <fgarcia@powereys.es>
2023-08-04 11:36:07 +0200

    Dibujos Vectoriales version lienzo gestor

---
+++ src/test/java/es/cic/ejerc005/TestLienzo.java
@@ -0,0 +1,80 @@
+package es.cic.ejerc005;
+
+import java.util.List;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.mock;
+
+public class TestLienzo {
+	
+
+    private LienzoImpl lienzo;
+    private Figura figuraMock;
+
+    @BeforeEach
+    public void setup() {
+        lienzo = new LienzoImpl();
+        figuraMock = mock(Figura.class);
+    }
+    
+    // Test para verificar que se agrega una figura al lienzo
+    @Test
+    public void testAgregarFigura() {
+        lienzo.agregarFigura(figuraMock);
+
+        List<Figura> figuras = lienzo.getFiguras();
+        assertThat(figuras, contains(figuraMock));
+    }
+    // Test para verificar que al actualizar una figura la lista la contiene
+    @Test
+    public void testActualizarFigura_ValidIndex() throws TipoFiguraDesconocidoException {
+        Figura figuraActualizadaMock = mock(Figura.class);
+
+        lienzo.agregarFigura(figuraMock);
+        lienzo.actualizarFigura(0, figuraActualizadaMock);
+
+        List<Figura> figuras = lienzo.getFiguras();
+        assertThat(figuras, contains(figuraActualizadaMock));
+    }
+   
+    // Test para comprobar invalida posicion
+    @Test
+    public void testActualizarFigura_InvalidPosition() {
+        Figura figuraActualizadaMock = mock(Figura.class);
+
+        lienzo.agregarFigura(figuraMock);
+        when(figuraActualizadaMock.getPosicionX()).thenReturn(100000); // Valor inválido
+
+    
+        List<Figura> figuras = lienzo.getFiguras();
+        assertThat(figuras, contains(figuraMock));
+    } 
+    // Test para comprobar que borra figuras y deja vacia la lista
+    @Test
+    public void testBorrarFigura_ValidIndex() throws TipoFiguraDesconocidoException {
+        lienzo.agregarFigura(figuraMock);
+        lienzo.borrarFigura(0);
+
+        List<Figura> figuras = lienzo.getFiguras();
+        assertThat(figuras, is(empty()));
+    }
+   
+    // Test para comprobar que mueve figura
+    @Test
+    public void testMoverFigura() {
+        int nuevaPosX = 300;
+        int nuevaPosY = 300;
+
+        lienzo.agregarFigura(figuraMock);
+        lienzo.moverFigura(0, nuevaPosX, nuevaPosY);
+
+        verify(figuraMock).setPosicionX(nuevaPosX);
+        verify(figuraMock).setPosicionY(nuevaPosY);
+    }
+
+  	   
+}
